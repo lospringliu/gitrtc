b07d709b01eef7323008cad3cd0d6c8450b7a643
Xinchun Liu <xcliu@ca.ibm.com>
2017-01-24 11:39:14 -0500

    delete baselineinstream branch when verifying

--- djangoapp.py
+++ djangoapp.py
@@ -259,13 +259,12 @@ if __name__ == '__main__':
 				shouter.shout("\t... stream %s has already been verified" % stream.name)
 				continue
 			rtcdir = os.path.join(RTCDIR,re.sub(r' ','',stream.name) + '_verify')
-			if os.path.exists(rtcdir):
-				shell.getoutput("rm -fr %s" % rtcdir )
-			ws_verify,created = Workspace.objects.get_or_create(name='git_verify_%s_%s' % (stream.component.name, stream.name))
-			ws_verify.component = stream.component
-			ws_verify.stream = stream
-			ws_verify.save()
-			rtc_initialize(rtcdir,gitdir=gitdir,workspace=ws_verify,component=stream.component,verifying=True)
+			if not os.path.exists(rtcdir):
+				ws_verify,created = Workspace.objects.get_or_create(name='git_verify_%s_%s' % (stream.component.name, stream.name))
+				ws_verify.component = stream.component
+				ws_verify.stream = stream
+				ws_verify.save()
+				rtc_initialize(rtcdir,gitdir=gitdir,workspace=ws_verify,component=stream.component,verifying=True)
 			for bis in stream.baselineinstream_set.all():
 				if bis.verified:
 					shouter.shout("\t... baseline in stream %s had been verified earlier" % bis.baseline.comment)
@@ -291,6 +290,12 @@ if __name__ == '__main__':
 					ws_verify.ws_set_component()
 					if bis.lastchangeset and bis.lastchangeset.commit:
 						try:
+							output = shell.getoutput("git -C %s branch" % rtcdir, clean=False)
+							if re.match(".*%s" % bis.baseline.uuid, output):
+								if git_got_changes(gitdir=rtcdir):
+									shell.getoutput("git -C %s commit -m test -a" % rtcdir, clean=False)
+								shell.getoutput("git -C %s checkout %s" % ( rtcdir, re.sub(r' ','',stream.name)))
+								shell.getoutput("git -C %s branch -D %s" % ( rtcdir, bis.baseline.uuid))
 							shell.getoutput("git -C %s checkout -b %s %s" % (rtcdir, bis.baseline.uuid, bis.lastchangeset.commit.commitid))
 							ws_verify.ws_load(load_dir=rtcdir)
 							shell.getoutput("git -C %s add -A" % rtcdir)
