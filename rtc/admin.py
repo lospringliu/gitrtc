fcb921d5e6090a548779fef390227e1a26a48a23
Xinchun Liu <xcliu@ca.ibm.com>
2020-06-30 17:15:28 -0400

    add search in admin page

--- rtc/admin.py
+++ rtc/admin.py
@@ -7,33 +7,38 @@ from rtc.models import *
 class JsonRecordAdmin(admin.ModelAdmin):
 	list_display = ('command','timestamp')
 	list_per_page =  100
-	save_on_top = True
 
 class ChangeSetAdmin(MPTTModelAdmin):
 	list_display = ('uuid','comment','migrated')
 	list_per_page = 100
+	search_fields = ('uuid','comment')
 
 class GitCommitAdmin(admin.ModelAdmin):
 	list_display = ('commitid','timestamp')
 	list_per_page = 100
+	search_fields = ('commitid',)
 
 class StreamAdmin(admin.ModelAdmin):
 	list_display = ('uuid','name')
 	list_per_page = 100
+	search_fields = ('uuid','name',)
 
 class SnapshotAdmin(admin.ModelAdmin):
 	list_display = ('uuid','migrated','name')
 	list_per_page = 100
 	list_filter=('stream',)
+	search_fields = ('name',)
 
 class BaselineAdmin(admin.ModelAdmin):
 	list_display = ('uuid','name')
 	list_per_page = 100
 	list_filter = ('snapshot',)
+	search_fields = ('name',)
 
 class WorkspaceAdmin(admin.ModelAdmin):
 	list_display = ('name', 'component', 'stream')
 	list_per_page = 300
+	search_fields = ('name','component__name','stream__name')
 
 admin.site.register(JsonRecord,JsonRecordAdmin)
 admin.site.register(ChangeSet,ChangeSetAdmin)
@@ -42,3 +47,5 @@ admin.site.register(Stream,StreamAdmin)
 admin.site.register(Baseline,BaselineAdmin)
 admin.site.register(Snapshot,SnapshotAdmin)
 admin.site.register(Workspace,WorkspaceAdmin)
+admin.site.register(ProjectArea)
+admin.site.register(GitBranch)
